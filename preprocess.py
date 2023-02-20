4a852774e82ba7c30a75fbc7eda43c63a9fefc00
Test <2460408813@qq.com>
2023-02-20 12:28:37 +0800

    qurope version

--- preprocess.py
+++ preprocess.py
@@ -2,12 +2,14 @@ import argparse
 import text
 from utils import load_filepaths_and_text
 
+# python preprocess.py --text_index 1 --filelists filelists\list.txt filelists\list_val.txt
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument("--out_extension", default="cleaned")
   parser.add_argument("--text_index", default=1, type=int)
   parser.add_argument("--filelists", nargs="+", default=["filelists/ljs_audio_text_val_filelist.txt", "filelists/ljs_audio_text_test_filelist.txt"])
-  parser.add_argument("--text_cleaners", nargs="+", default=["english_cleaners2"])
+  parser.add_argument("--text_cleaners", nargs="+", default=["japanese_cleaners"])
 
   args = parser.parse_args()
     
--- train.py
+++ train.py
@@ -16,6 +16,11 @@ from torch.cuda.amp import autocast, GradScaler
 import librosa
 import logging
 
+# python train.py -c configs\config.json -m EModel
+"""
+私はヨークタウン型航空母艦のエンタープライズ.グレイゴーストは私が持つ数多くの異名の中の一つだ.この硝煙と鮮血の戦歴こそ我が使命の証.これからも戦いの果てまで勝ち続けて見せる
+"""
+
 logging.getLogger('numba').setLevel(logging.WARNING)
 
 import commons
@@ -64,7 +69,7 @@ def run(rank, n_gpus, hps):
     writer = SummaryWriter(log_dir=hps.model_dir)
     writer_eval = SummaryWriter(log_dir=os.path.join(hps.model_dir, "eval"))
 
-  dist.init_process_group(backend='nccl', init_method='env://', world_size=n_gpus, rank=rank)
+  dist.init_process_group(backend='gloo', init_method='env://', world_size=n_gpus, rank=rank)
   torch.manual_seed(hps.train.seed)
   torch.cuda.set_device(rank)
 
@@ -101,8 +106,8 @@ def run(rank, n_gpus, hps):
       hps.train.learning_rate, 
       betas=hps.train.betas, 
       eps=hps.train.eps)
-  net_g = DDP(net_g, device_ids=[rank])
-  net_d = DDP(net_d, device_ids=[rank])
+  net_g = DDP(net_g, device_ids=[rank], find_unused_parameters=True)
+  net_d = DDP(net_d, device_ids=[rank], find_unused_parameters=True)
 
   try:
     _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "G_*.pth"), net_g, optim_g)
